2279fae7d5f9d2a628d112218a95cf8d8de50e9a
Samuel DIDIER-LAURENT <istuffs@gmail.com>
2018-12-11 15:45:27 +0100

    feature: minifing
    - add mjml engine minification
    - add imagemin to minifi images

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 const gulp = require('gulp');
 const mjml = require('gulp-mjml');
+const mjmlEngine = require('mjml');
 const browserSync = require('browser-sync');
+const imagemin = require('gulp-imagemin');
 
+const CONFIG = require('./config.json');
 
 function htmlTask () {
-    return gulp.src('./src/*.mjml')
-    .pipe(mjml())
+    return gulp.src('./src/**/*.mjml')
+    .pipe(mjml(mjmlEngine, CONFIG.mjml))
     .pipe(gulp.dest('./dist'));
+}
+
+function imgTask() {
+    return gulp.src('./src/assets/imgages/**/*.{gif,jpg,png,svg,jpeg}')
+        .pipe(imagemin({
+            verbose: true
+        }))
+        .pipe(gulp.dest('dist/assets/imgages'));
 }
 
 function refresh(done) {
@@ -21,6 +32,7 @@ function refresh(done) {
 
 gulp.task('watch', gulp.series(htmlTask, refresh, function() {
     gulp.watch('./src/*.mjml', gulp.series(htmlTask));
+    gulp.watch('./dist/**/*.html', gulp.series(imgTask));
     gulp.watch('./dist/*.html').on('change', browserSync.reload);
 }));
 
